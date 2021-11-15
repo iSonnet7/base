fb3afba35a240e24ca74995e077207c6b79a670d
iSonnet7 <paco82009@gmail.com>
2021-11-15 14:53:59 -0600

    Ok v4

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,6 @@
+import 'package:base/screens/login.dart';
+import 'package:base/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -7,15 +10,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final user = FirebaseAuth.instance;
+  AuthService auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text("Hello World"),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountName: Text('Usuario'), accountEmail: Text(user.currentUser!.email!),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: new Image.asset("assets/profile.png"),
+                ),
+            ),
+            ListTile(
+              title: Text('Perfil'),
+              leading: Icon(
+                  Icons.account_circle_outlined,
+                color: Colors.cyan,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Resultados'),
+              leading: Icon(
+                Icons.assessment_outlined,
+                color: Colors.cyan,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Salir'),
+              leading: Icon(
+                Icons.logout_outlined,
+                color: Colors.cyan,
+              ),
+              onTap: () {
+                auth.singOut();
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+              },
+            )
+          ],
         ),
       ),
     );
   }
 }
-
